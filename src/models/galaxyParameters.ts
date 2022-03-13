52df337a36f544fe198e204cdb6bb2da99b6a8b8
Travis Craft <tlcraft@users.noreply.github.com>
2022-03-13 12:00:27 -0600

    Add interface.

---
+++ src/models/galaxyParameters.ts
@@ -0,0 +1,11 @@
+export interface GalaxyParameters {
+    branches: number;
+    count: number;
+    insideColor: string;
+    outsideColor: string;
+    radius: number;
+    randomness: number;
+    randomnessPower: number;
+    size: number;
+    spin: number;
+}
